a201fd4ae8842da2e7f18b470efd8a4991598d2c
putrinurifa <putrinurifa818@gmail.com>
2021-03-01 18:24:32 +0700

    HOME

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,73 +1,116 @@
-@extends('layouts.footer')
-@extends('layouts.content')
+@extends('layouts.app')
 @section('content')
-<head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-
-        <title>Putri&Rakha</title>
-
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
-
-        <!-- Styles -->
-        <style>
-            /*! normalize.css v8.0.1 | MIT License | github.com/necolas/normalize.css */html{line-height:1.15;-webkit-text-size-adjust:100%}body{margin:0}a{background-color:transparent}[hidden]{display:none}html{font-family:system-ui,-apple-system,BlinkMacSystemFont,Segoe UI,Roboto,Helvetica Neue,Arial,Noto Sans,sans-serif,Apple Color Emoji,Segoe UI Emoji,Segoe UI Symbol,Noto Color Emoji;line-height:1.5}*,:after,:before{box-sizing:border-box;border:0 solid #e2e8f0}a{color:inherit;text-decoration:inherit}svg,video{display:block;vertical-align:middle}video{max-width:100%;height:auto}.bg-white{--bg-opacity:1;background-color:#fff;background-color:rgba(255,255,255,var(--bg-opacity))}.bg-gray-100{--bg-opacity:1;background-color:#f7fafc;background-color:rgba(247,250,252,var(--bg-opacity))}.border-gray-200{--border-opacity:1;border-color:#edf2f7;border-color:rgba(237,242,247,var(--border-opacity))}.border-t{border-top-width:1px}.flex{display:flex}.grid{display:grid}.hidden{display:none}.items-center{align-items:center}.justify-center{justify-content:center}.font-semibold{font-weight:600}.h-5{height:1.25rem}.h-8{height:2rem}.h-16{height:4rem}.text-sm{font-size:.875rem}.text-lg{font-size:1.125rem}.leading-7{line-height:1.75rem}.mx-auto{margin-left:auto;margin-right:auto}.ml-1{margin-left:.25rem}.mt-2{margin-top:.5rem}.mr-2{margin-right:.5rem}.ml-2{margin-left:.5rem}.mt-4{margin-top:1rem}.ml-4{margin-left:1rem}.mt-8{margin-top:2rem}.ml-12{margin-left:3rem}.-mt-px{margin-top:-1px}.max-w-6xl{max-width:72rem}.min-h-screen{min-height:100vh}.overflow-hidden{overflow:hidden}.p-6{padding:1.5rem}.py-4{padding-top:1rem;padding-bottom:1rem}.px-6{padding-left:1.5rem;padding-right:1.5rem}.pt-8{padding-top:2rem}.fixed{position:fixed}.relative{position:relative}.top-0{top:0}.right-0{right:0}.shadow{box-shadow:0 1px 3px 0 rgba(0,0,0,.1),0 1px 2px 0 rgba(0,0,0,.06)}.text-center{text-align:center}.text-gray-200{--text-opacity:1;color:#edf2f7;color:rgba(237,242,247,var(--text-opacity))}.text-gray-300{--text-opacity:1;color:#e2e8f0;color:rgba(226,232,240,var(--text-opacity))}.text-gray-400{--text-opacity:1;color:#cbd5e0;color:rgba(203,213,224,var(--text-opacity))}.text-gray-500{--text-opacity:1;color:#a0aec0;color:rgba(160,174,192,var(--text-opacity))}.text-gray-600{--text-opacity:1;color:#718096;color:rgba(113,128,150,var(--text-opacity))}.text-gray-700{--text-opacity:1;color:#4a5568;color:rgba(74,85,104,var(--text-opacity))}.text-gray-900{--text-opacity:1;color:#1a202c;color:rgba(26,32,44,var(--text-opacity))}.underline{text-decoration:underline}.antialiased{-webkit-font-smoothing:antialiased;-moz-osx-font-smoothing:grayscale}.w-5{width:1.25rem}.w-8{width:2rem}.w-auto{width:auto}.grid-cols-1{grid-template-columns:repeat(1,minmax(0,1fr))}@media (min-width:640px){.sm\:rounded-lg{border-radius:.5rem}.sm\:block{display:block}.sm\:items-center{align-items:center}.sm\:justify-start{justify-content:flex-start}.sm\:justify-between{justify-content:space-between}.sm\:h-20{height:5rem}.sm\:ml-0{margin-left:0}.sm\:px-6{padding-left:1.5rem;padding-right:1.5rem}.sm\:pt-0{padding-top:0}.sm\:text-left{text-align:left}.sm\:text-right{text-align:right}}@media (min-width:768px){.md\:border-t-0{border-top-width:0}.md\:border-l{border-left-width:1px}.md\:grid-cols-2{grid-template-columns:repeat(2,minmax(0,1fr))}}@media (min-width:1024px){.lg\:px-8{padding-left:2rem;padding-right:2rem}}@media (prefers-color-scheme:dark){.dark\:bg-gray-800{--bg-opacity:1;background-color:#2d3748;background-color:rgba(45,55,72,var(--bg-opacity))}.dark\:bg-gray-900{--bg-opacity:1;background-color:#1a202c;background-color:rgba(26,32,44,var(--bg-opacity))}.dark\:border-gray-700{--border-opacity:1;border-color:#4a5568;border-color:rgba(74,85,104,var(--border-opacity))}.dark\:text-white{--text-opacity:1;color:#fff;color:rgba(255,255,255,var(--text-opacity))}.dark\:text-gray-400{--text-opacity:1;color:#cbd5e0;color:rgba(203,213,224,var(--text-opacity))}}
-        </style>
-
-        <style>
-            body {
-                font-family: 'Nunito';
-            }
-        </style>
-    </head>
-    <div class="container">
-        <img src="https://pngimage.net/wp-content/uploads/2018/06/gambar-kartun-anak-sekolah-png-1.png" class="img-fluid w-100" alt="...">  
-        <div class="row align-items-start bg-light">
-            <div class="col">
-                <div class="card" style="width: 18rem;">
-                    <img src="https://faktualnews.co/images/2019/07/ilustrasi-gambar-alat-game_1.jpg" class="card-img-top" alt="...">
-                        <div class="card-body">
-                            <h5 class="card-title">Edu Games</h5>
-                            <p class="card-text">Marbel Educational Games</p>
-                            <a href="products/eduGames" class="btn btn-primary">Edu Games</a>
-                        </div>
+<!-- Page Content -->
+    <!-- Banner Starts Here -->
+    <div class="main-banner header-text">
+      <div class="container-fluid">
+        <div class="owl-banner owl-carousel">
+          <div class="item">
+            <img src="{{asset('https://th.bing.com/th/id/Re3504a8b6534e5b52c54df85433639b1?rik=ioYrd%2fmC1Lr40g&riu=http%3a%2f%2fwww.saintcatherinespreschool.org%2fuploads%2f3%2f1%2f1%2f3%2f31135279%2fedited%2fcartoon-kids-wear-masks-119631-265.jpg&ehk=KRs66ViLPmWLBcvNB0vFvj%2f9ySo4llyWfuK6HOGx90w%3d&risl=&pid=ImgRaw')}}" alt="banner image">
+          </div>
+          <div class="item">
+            <img src="{{asset('https://pngimage.net/wp-content/uploads/2018/06/gambar-kartun-anak-sekolah-png-1.png')}}" alt="">
+            <div class="item-content">
+              <div class="main-content">
+                <div class="meta-category">
                 </div>
+              </div>
             </div>
-            <div class="col">
-            <div class="card" style="width: 18rem;">
-                    <img src="https://th.bing.com/th/id/OIP.rvaOf5WLgeBRFnl7j015eQAAAA?pid=ImgDet&rs=1" class="card-img-top" alt="...">
-                        <div class="card-body">
-                            <h5 class="card-title">Kids Games</h5>
-                            <p class="card-text"> Marbel & Friends - Kids Games.</p>
-                            <a href="products/kidsGames" class="btn btn-primary">Kids Games</a>
-                        </div>
+          </div>
+          <div class="item">
+            <img src="{{asset('https://th.bing.com/th/id/OIP.rvaOf5WLgeBRFnl7j015eQAAAA?pid=ImgDet&rs=1')}}" alt="">
+            <div class="item-content">
+              <div class="main-content">
+                <div class="meta-category">
                 </div>
+              </div>
             </div>
-            <div class="col">
-            <div class="card" style="width: 18rem;">
-                    <img src="https://th.bing.com/th/id/R89033d73778e12f2e8d12d886bb706d6?rik=k3kBo%2bAP4bWRAA&riu=http%3a%2f%2fmylittlebookshop.in%2fwp-content%2fuploads%2f2016%2f09%2fWhatsApp-Image-2016-09-13-at-23.43.01-1080x675.jpeg&ehk=FkgfTq9XR8tUACnKLE%2flw1H1DxD3BTviMixMVBdFt5M%3d&risl=&pid=ImgRaw" class="card-img-top" alt="...">
-                        <div class="card-body">
-                            <h5 class="card-title">Stroy Books</h5>
-                            <p class="card-text">Riri Story Books</p>
-                            <a href="products/story" class="btn btn-primary">Story Books</a>
-                        </div>
+          </div>
+          <div class="item">
+            <img src="{{asset('https://wennykinanthi.files.wordpress.com/2018/04/anak-tk.png')}}" alt="">
+            <div class="item-content">
+              <div class="main-content">
+                <div class="meta-category">
                 </div>
+              </div>
             </div>
+          </div>
         </div>
-        <div class="row align-items-center bg-light">
-            <div class="col">
-            <div class="card" style="width: 18rem;">
-                    <img src="https://multimedia-english.com/img/web/covers/kids-songs.jpg" class="card-img-top" alt="...">
-                        <div class="card-body">
-                            <h5 class="card-title">Songs</h5>
-                            <p class="card-text">Kolak Kids Songs</p>
-                            <a href="products/songs" class="btn btn-primary">Kids Songs</a>
-                        </div>
-                </div>
-            </div>
-        </div>
+      </div>
     </div>
-@endsection
-@extends('layouts.sidebar')
-@extends('layouts.header')
+    <!-- Banner Ends Here -->
+
+    <section class="call-to-action">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-12">
+            <div class="main-content">
+              <div class="row">
+                <div class="col-lg-8">
+                  <span>JOBSHEET 2 PRAKTIKUM 2</span>
+                  <h4>TEAM PUTRI & RAKHA</h4>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section class="blog-posts">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-8">
+            <div class="all-blog-posts">
+              <div class="row">
+                <div class="col-lg-12">
+                  <div class="blog-post">
+                    <div class="blog-thumb">
+                      <img src="{{asset('https://image.jimcdn.com/app/cms/image/transf/none/path/s7e15da4d60fb7de8/backgroundarea/ibb777fd755bbf7ba/version/1516732684/image.png')}}" alt="">
+                    </div>
+                    <div class="down-content">
+                      <span>Edu Games</span>
+                      <a href="https://www.educastudio.com/category/marbel-edu-games"><h4>Marbel - Educational Games</h4></a>
+                    </div>
+                  </div>
+                </div>
+                <div class="col-lg-12">
+                  <div class="blog-post">
+                    <div class="blog-thumb">
+                      <img src="{{asset('https://th.bing.com/th/id/Ra53e50c70b71c072c6270774cd417e88?rik=r3Px8AJ7B6Fv1w&riu=http%3a%2f%2fchidionye.com%2fimg%2fplayground.png&ehk=iTJIqSVn6DlHNo%2by9CepgLJGu2lr1LGA0UvkTQ8O%2fYA%3d&risl=&pid=ImgRaw')}}" alt="">
+                    </div>
+                    <div class="down-content">
+                    <span>Kids Games</span>
+                      <a href="https://www.educastudio.com/category/marbel-and-friends-kids-games"><h4>Marbel & Friends - Kids Games</h4></a>
+                    </div>
+                  </div>
+                </div>
+                <div class="col-lg-12">
+                  <div class="blog-post">
+                    <div class="blog-thumb">
+                      <img src="{{asset('https://clipartart.com/images/book-care-clipart-2.png')}}" alt="">
+                    </div>
+                    <div class="down-content">
+                      <span>Story Books</span>
+                      <a href="https://www.educastudio.com/category/riri-story-books"><h4>Riri - Story Books</h4></a>
+                    </div>
+                  </div>
+                </div>
+                <div class="col-lg-12">
+                  <div class="blog-post">
+                    <div class="blog-thumb">
+                      <img src="{{asset('https://multimedia-english.com/img/web/covers/kids-songs.jpg')}}" alt="">
+                    </div>
+                    <div class="down-content">
+                      <span>Kids Songs</span>
+                      <a href="https://www.educastudio.com/category/riri-story-books"><h4>Riri - Songs</h4></a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,100 +1,124 @@
-<!doctype html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-<head>
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="TemplateMo">
+    <link href="https://fonts.googleapis.com/css?family=Roboto:100,100i,300,300i,400,400i,500,500i,700,700i,900,900i&display=swap" rel="stylesheet">
 
-    <!-- CSRF Token -->
-    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <title>Praktikum 2</title>
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <!-- Bootstrap core CSS -->
+    <link href="{{asset('vendor/bootstrap/css/bootstrap.min.css')}}" rel="stylesheet">
 
-    <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
+    <!-- Additional CSS Files -->
+    <link rel="stylesheet" href="{{asset('css/fontawesome.css')}}">
+    <link rel="stylesheet" href="{{asset('css/templatemo-stand-blog.css')}}">
+    <link rel="stylesheet" href="{{asset('css/owl.css')}}">
+<!--
 
-    <!-- Fonts -->
-    <link rel="dns-prefetch" href="//fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+TemplateMo 551 Stand Blog
 
-    <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-</head>
-<body>
-    <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
-            <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    <b>PUTRI DAN RAKHA</b>
+https://templatemo.com/tm-551-stand-blog
+
+-->
+  </head>
+
+  <body>
+
+    <!-- ***** Preloader Start ***** -->
+    <div id="preloader">
+        <div class="jumper">
+            <div></div>
+            <div></div>
+            <div></div>
+        </div>
+    </div>  
+    <!-- ***** Preloader End ***** -->
+
+    <!-- Header -->
+    <header class="">
+      <nav class="navbar navbar-expand-lg">
+        <div class="container">
+          <a class="navbar-brand" href="http://127.0.0.1:8000/"><h2>PUTRI & RAKHA<em>.</em></h2></a>
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarResponsive">
+            <ul class="navbar-nav ml-auto">
+              <li class="nav-item active">
+                <a class="nav-link" href="http://127.0.0.1:8000/">Home
+                  <span class="sr-only">(current)</span>
                 </a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
+              </li> 
+              <li class="nav-item">
+                <a class="nav-link" href="about.html">About Us</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="blog.html">Product</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="post-details.html">Program</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="post-details.html">News</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="contact.html">Contact Us</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </header>
+    @yield('content')
 
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav mr-auto">
-                       <li class="nav-item">
-                       <a href="{{ url('/home') }}" class="text-sm text-gray-700 underline">Home</a>
-                        </li>
-                        <li class="nav-item">
-                        <a href="{{ url('/products') }}" class="text-sm text-gray-700 underline">Products</a>
-                        </li> 
-                        <li class="nav-item">
-                            <a href="{{ url('/news') }}" class="text-sm text-gray-700 underline">News</a>
-                        </li>
-                        <li class="nav-item">
-                            <a href="{{ url('/programs') }}" class="text-sm text-gray-700 underline">Programs</a>
-                        </li>
-                        <li href="{{ url('/about-us') }}" class="text-sm text-gray-700 underline">
-                            <a class="nav-link">About-Us</a>
-                        </li>
-                        <li href="{{ url('/contact-us') }}" class="text-sm text-gray-700 underline">
-                            <a class="nav-link">Contact-Us</a>
-                        </li>
-                    </ul>
-
-                    <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ml-auto">
-                        <!-- Authentication Links -->
-                        @guest
-                            @if (Route::has('login'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                                </li>
-                            @endif
-                            
-                            @if (Route::has('register'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                                </li>
-                            @endif
-                        @else
-                            <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }}
-                                </a>
-
-                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
-                                    </a>
-
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                                        @csrf
-                                    </form>
-                                </div>
-                            </li>
-                        @endguest
-                    </ul>
-                </div>
+    <footer>
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-12">
+            <ul class="social-icons">
+              <li><a href="#">Facebook</a></li>
+              <li><a href="#">Twitter</a></li>
+              <li><a href="#">Behance</a></li>
+              <li><a href="#">Linkedin</a></li>
+              <li><a href="#">Dribbble</a></li>
+            </ul>
+          </div>
+          <div class="col-lg-12">
+            <div class="copyright-text">
+              <p>Copyright 2021 Putri&Rakha.
+                    
+                 | Design: <a rel="nofollow" href="https://templatemo.com" target="_parent">TemplateMo</a></p>
             </div>
-        </nav>
+          </div>
+        </div>
+      </div>
+    </footer>
 
-        <main class="py-4">
-            @yield('content')
-        </main>
-    </div>
-</body>
+    <!-- Bootstrap core JavaScript -->
+    <script src="{{asset('vendor/jquery/jquery.min.js')}}"></script>
+    <script src="{{asset('vendor/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
+
+    <!-- Additional Scripts -->
+    <script src="{{asset('js/custom.js')}}"></script>
+    <script src="{{asset('js/owl.js')}}"></script>
+    <script src="{{asset('js/slick.js')}}"></script>
+    <script src="{{asset('js/isotope.js')}}"></script>
+    <script src="{{asset('js/accordions.js')}}"></script>
+
+    <script language = "text/Javascript"> 
+      cleared[0] = cleared[1] = cleared[2] = 0; //set a cleared flag for each field
+      function clearField(t){                   //declaring the array outside of the
+      if(! cleared[t.id]){                      // function makes it static and global
+          cleared[t.id] = 1;  // you could use true and false, but that's more typing
+          t.value='';         // with more chance of typos
+          t.style.color='#fff';
+          }
+      }
+    </script>
+  </body>
 </html>
